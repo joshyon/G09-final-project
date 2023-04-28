9199448f8ce1e6be2e6215f754947f494d6d4fdf
joshyon <joshyon>
2023-04-28 10:49:07 +0000

    hourly_trip_info_creation
    a silver table contains feature for ML is created after streaming

--- final_project/01 etl.py
+++ final_project/01 etl.py
@@ -161,6 +161,51 @@ spark.readStream.format("delta").load(BRONZE_NYC_WEATHER_PATH).createOrReplaceTe
 
 # COMMAND ----------
 
+from pyspark.sql.functions import col, window, count, when, sum
+from pyspark.sql.functions import dayofweek
+from pyspark.sql.functions import from_unixtime
+
+weather_data = spark.read.table("bronze_historic_weather_data")
+weather_data = weather_data.withColumn('date_timestamp', from_unixtime('dt'))
+# weather_data.show()
+bike_trip_info = spark.read.table("bronze_historic_bike_trip").filter((col("start_station_name") == "E 33 St & 1 Ave") | (col("end_station_name") == "E 33 St & 1 Ave")).orderBy(col("started_at").asc())
+# bike_trip_info.show(n=1000)
+
+# COMMAND ----------
+
+hourly_trip_data = bike_trip_info.groupBy(window(col("started_at"), "1 hour"), col("start_station_name") == "E 33 St & 1 Ave").agg(count("*").alias("count")).orderBy(col("window").asc())
+hourly_trip_data = hourly_trip_data.withColumnRenamed("(start_station_name = E 33 St & 1 Ave)", "indicator")
+
+
+success_count = sum(when(col("indicator") == True, col("count")))
+
+# Define a conditional expression to count error statuses
+error_count = sum(when(col("indicator") == False, col("count")))
+
+# Group by hour and apply the conditional expressions
+hourly_trip_data = hourly_trip_data.groupBy("window").agg(success_count.alias("true"), error_count.alias("false")).orderBy(col("window").asc()).na.fill(0)
+
+
+hourly_trip_data = hourly_trip_data.withColumnRenamed("window", "window").withColumnRenamed("false", "bikes_returning").withColumnRenamed("true", "bikes_leaving")
+hourly_trip_data = hourly_trip_data.withColumn("bikes_net_change", hourly_trip_data["bikes_returning"]-hourly_trip_data["bikes_leaving"])
+hourly_trip_data = hourly_trip_data.withColumn("date_timestamp", hourly_trip_data["window"]["start"])
+hourly_trip_data = hourly_trip_data.withColumn("weekday_indicator", (dayofweek(hourly_trip_data["date_timestamp"]) >= 2) & (dayofweek(hourly_trip_data["date_timestamp"]) <= 6))
+hourly_trip_data = hourly_trip_data.drop("window")
+
+# merge with weather dataframe
+join_df = weather_data.select("date_timestamp", "temp", "snow_1h")
+hourly_trip_data = hourly_trip_data.join(join_df, "date_timestamp", "left").orderBy(col("date_timestamp").asc())
+
+hourly_trip_data.show(n=5000)
+
+
+# COMMAND ----------
+
+hourly_trip_data.write.mode("overwrite").option("header", "true").csv("dbfs:/FileStore/tables/G09/hourly_trip_data")
+
+
+# COMMAND ----------
+
 import json
 
 # Return Success
