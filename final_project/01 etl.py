6934221dcc1625d86fb593477ecd182614195fed
joshyon <joshyon>
2023-05-04 21:49:20 +0000

    Silver tables now streaming and added Zorder & partition to bronze

--- final_project/01 etl.py
+++ final_project/01 etl.py
@@ -3,6 +3,13 @@
 
 # COMMAND ----------
 
+dbutils.widgets.removeAll()
+
+dbutils.widgets.text('01.start_date', "2021-10-01")
+dbutils.widgets.text('02.end_date', "2023-03-01")
+dbutils.widgets.text('03.hours_to_forecast', '4')
+dbutils.widgets.text('04.promote_model', 'No')
+
 start_date = str(dbutils.widgets.get('01.start_date'))
 end_date = str(dbutils.widgets.get('02.end_date'))
 hours_to_forecast = int(dbutils.widgets.get('03.hours_to_forecast'))
@@ -45,6 +52,7 @@ historic_bike_df = (spark.readStream
 
 #This cell completes the writeStream for the historic_bike_data
 (historic_bike_df.writeStream
+ .partitionBy("start_station_name")
  .option("checkpointLocation", f"{GROUP_DATA_PATH}/bronze/historic_bike/checkpoints")
  .option("mergeSchema", "true")
  .outputMode("append")
@@ -53,14 +61,20 @@ historic_bike_df = (spark.readStream
  .start(f"{GROUP_DATA_PATH}/bronze_historic_bike_trip.delta")
 )
 
-#change the toTable command to .save(GROUP_DATA_PATH + delta_table_name.delta)
+
+
+# COMMAND ----------
+
+#saves bronze historic bike trip data as a managed table in our group database.
+
+historic_bike_data_table = spark.read.format("delta").load(f"{GROUP_DATA_PATH}/bronze_historic_bike_trip.delta")
+historic_bike_data_table.write.format("delta").mode("overwrite").option("overwriteSchema", "true").saveAsTable("G09_db.bronze_historic_bike_trip")
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC CREATE TABLE IF NOT EXISTS bronze_historic_bike_trip
-# MAGIC   USING delta
-# MAGIC     LOCATION 'dbfs:/FileStore/tables/G09/bronze_historic_bike_trip.delta/'
+# MAGIC OPTIMIZE bronze_historic_bike_trip
+# MAGIC ZORDER BY (started_at)
 
 # COMMAND ----------
 
@@ -105,6 +119,7 @@ historic_weather_df = (spark.readStream
 
 #This is the writeStream for the historic_weather_data
 (historic_weather_df.writeStream
+ .partitionBy("main")
  .option("checkpointLocation", f"{GROUP_DATA_PATH}/bronze/historic_weather/checkpoints")
  .option("mergeSchema", "true")
  .outputMode("append")
@@ -117,10 +132,16 @@ historic_weather_df = (spark.readStream
 
 # COMMAND ----------
 
+#saves bronze historic weather data as a managed table in our group database.
+
+historic_weather_table = spark.read.format("delta").load(f"{GROUP_DATA_PATH}/bronze_historic_weather.delta")
+historic_weather_table.write.format("delta").mode("overwrite").option("overwriteSchema", "true").saveAsTable("G09_db.bronze_historic_weather_data")
+
+# COMMAND ----------
+
 # MAGIC %sql
-# MAGIC CREATE TABLE IF NOT EXISTS bronze_historic_weather_data
-# MAGIC  USING delta
-# MAGIC     LOCATION 'dbfs:/FileStore/tables/G09/bronze_historic_weather.delta/'
+# MAGIC OPTIMIZE bronze_historic_weather_data
+# MAGIC ZORDER BY (dt)
 
 # COMMAND ----------
 
@@ -129,48 +150,130 @@ historic_weather_df = (spark.readStream
 # COMMAND ----------
 
 from pyspark.sql.functions import col, window, count, when, sum
-from pyspark.sql.functions import dayofweek
-from pyspark.sql.functions import from_unixtime
 
-weather_data = spark.read.load("dbfs:/FileStore/tables/G09/bronze_historic_weather.delta")
 
-weather_data = weather_data.withColumn('date_timestamp', from_unixtime('dt'))
-# weather_data.show()
-bike_trip_info = spark.read.load("dbfs:/FileStore/tables/G09/bronze_historic_bike_trip.delta").filter((col("start_station_name") == "E 33 St & 1 Ave") | (col("end_station_name") == "E 33 St & 1 Ave")).orderBy(col("started_at").asc())
-# bike_trip_info.show(n=1000)
+bike_trip_info = (
+    spark
+    .readStream.format("delta")
+    .option("maxFilesPerTrigger", 1)
+    .load("dbfs:/FileStore/tables/G09/bronze_historic_bike_trip.delta")
+)
+
+
+streamingDF = (
+    bike_trip_info
+    .filter((col("start_station_name") == "E 33 St & 1 Ave") | (col("end_station_name") == "E 33 St & 1 Ave"))
+    .withWatermark("started_at", "2 hours")
+    .groupBy(window(col("started_at"), "1 hour"), col("start_station_name") == "E 33 St & 1 Ave").agg(count("*").alias("count"))
+    .withColumnRenamed("(start_station_name = E 33 St & 1 Ave)", "indicator")
+)
+
+checkpoint_path = f"{GROUP_DATA_PATH}/silver/silver_indicator_df/checkpoint"
+output_path = f"{GROUP_DATA_PATH}/silver_indicator_df.delta"
+
+devices_query = (
+                streamingDF
+                 .writeStream
+                 .outputMode("append")
+                 .format("delta")
+                 .queryName("write_silver_indicator_df")
+                 .trigger(availableNow=True)
+                 .option("checkpointLocation", checkpoint_path)
+                 .start(output_path)
+                )
 
 # COMMAND ----------
 
-hourly_trip_data = bike_trip_info.groupBy(window(col("started_at"), "1 hour"), col("start_station_name") == "E 33 St & 1 Ave").agg(count("*").alias("count")).orderBy(col("window").asc())
-hourly_trip_data = hourly_trip_data.withColumnRenamed("(start_station_name = E 33 St & 1 Ave)", "indicator")
+from pyspark.sql.functions import dayofweek
 
+silver_indicator_df = (
+    spark
+    .readStream.format("delta")
+    .option("maxFilesPerTrigger", 1)
+    .load("dbfs:/FileStore/tables/G09/silver_indicator_df.delta")
+)
 
 success_count = sum(when(col("indicator") == True, col("count")))
 
-# Define a conditional expression to count error statuses
 error_count = sum(when(col("indicator") == False, col("count")))
 
-# Group by hour and apply the conditional expressions
-hourly_trip_data = hourly_trip_data.groupBy("window").agg(success_count.alias("true"), error_count.alias("false")).orderBy(col("window").asc()).na.fill(0)
+streaming_silver_indicatorDF = (
+    silver_indicator_df
+    .groupBy("window").agg(success_count.alias("true"), error_count.alias("false"))
+    .na.fill(0)
+    .withColumnRenamed("window", "window")
+    .withColumnRenamed("false", "bikes_returning")
+    .withColumnRenamed("true", "bikes_leaving")
+    .withColumn("bikes_net_change", col("bikes_returning")-col("bikes_leaving"))
+    .withColumn("date_timestamp", col("window")["start"])
+    .withColumn("weekday_indicator", (dayofweek(col("date_timestamp")) >= 2) & (dayofweek(col("date_timestamp")) <= 6))
+    .drop("window")
+)
 
+checkpoint_path = f"{GROUP_DATA_PATH}/silver/silver_hourly_raw/checkpoint"
+output_path = f"{GROUP_DATA_PATH}/silver_hourly_raw.delta"
 
-hourly_trip_data = hourly_trip_data.withColumnRenamed("window", "window").withColumnRenamed("false", "bikes_returning").withColumnRenamed("true", "bikes_leaving")
-hourly_trip_data = hourly_trip_data.withColumn("bikes_net_change", hourly_trip_data["bikes_returning"]-hourly_trip_data["bikes_leaving"])
-hourly_trip_data = hourly_trip_data.withColumn("date_timestamp", hourly_trip_data["window"]["start"])
-hourly_trip_data = hourly_trip_data.withColumn("weekday_indicator", (dayofweek(hourly_trip_data["date_timestamp"]) >= 2) & (dayofweek(hourly_trip_data["date_timestamp"]) <= 6))
-hourly_trip_data = hourly_trip_data.drop("window")
-
-# merge with weather dataframe
-join_df = weather_data.select("date_timestamp", "temp", "snow_1h")
-hourly_trip_data = hourly_trip_data.join(join_df, "date_timestamp", "left").orderBy(col("date_timestamp").asc())
-
-hourly_trip_data.show(n=5000)
+silver_query = (
+                streaming_silver_indicatorDF
+                 .writeStream
+                 .outputMode("complete")
+                 .format("delta")
+                 .queryName("silver_query")
+                 .trigger(availableNow=True)
+                 .option("checkpointLocation", checkpoint_path)
+                 .start(output_path)
+                )
 
 
 # COMMAND ----------
 
-hourly_trip_data.write.mode("overwrite").option("header", "true").csv("dbfs:/FileStore/tables/G09/hourly_trip_data")
+from pyspark.sql.functions import from_unixtime
 
+weather_data = (
+    spark
+    .readStream
+    .format("delta")
+    .option("path", "dbfs:/FileStore/tables/G09/bronze_historic_weather.delta")
+    .load()
+)
+
+hourly_silver_data = (
+    spark
+    .readStream
+    .format("delta")
+    .option("path", "dbfs:/FileStore/tables/G09/silver_hourly_raw.delta")
+    .load()
+)
+
+weather_join_data = (
+    weather_data
+    .withColumn('date_timestamp', from_unixtime('dt'))
+    .select("date_timestamp", "temp", "snow_1h", "pop")
+)
+
+silver_hourly_trip = (
+    hourly_silver_data.join(weather_join_data, "date_timestamp")
+    .na.drop()
+)
+
+checkpoint_path = f"{GROUP_DATA_PATH}/silver/silver_hourly_trip_info/checkpoint"
+output_path = f"{GROUP_DATA_PATH}/silver_hourly_trip_info.delta"
+
+final_query = (
+    silver_hourly_trip
+    .writeStream
+    .format("delta")
+    .trigger(availableNow=True)
+    .queryName("final_query")
+    .outputMode("append")
+    .option("checkpointLocation", checkpoint_path)
+    .start(output_path)
+)
+
+# COMMAND ----------
+
+silver_hourly_trip_table = spark.read.format('delta').load(f"{GROUP_DATA_PATH}/silver_hourly_trip_info.delta")
+silver_hourly_trip_table.show()
 
 # COMMAND ----------
 
