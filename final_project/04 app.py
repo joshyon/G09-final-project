c1a84011e122c2ced942ee386b785e37999c0e76
cmiao3@u.rochester.edu <cmiao3@u.rochester.edu>
2023-05-06 01:30:56 +0000

    final commit

--- final_project/04 app.py
+++ final_project/04 app.py
@@ -4,21 +4,292 @@
 # COMMAND ----------
 
 # DBTITLE 0,YOUR APPLICATIONS CODE HERE...
-start_date = str(dbutils.widgets.get('01.start_date'))
-end_date = str(dbutils.widgets.get('02.end_date'))
+import json
+import pandas as pd
+import mlflow
+import mlflow.pyfunc
+import datetime
+import pytz
+import matplotlib.pyplot as plt
+from prophet import Prophet
+from prophet.serialize import model_from_json
+
 hours_to_forecast = int(dbutils.widgets.get('03.hours_to_forecast'))
-promote_model = bool(True if str(dbutils.widgets.get('04.promote_model')).lower() == 'yes' else False)
 
-print(start_date,end_date,hours_to_forecast, promote_model)
-
-print("YOUR CODE HERE...")
+# Read bronze streaming delta table
+trip_data = spark.read.format("delta").load("dbfs:/FileStore/tables/bronze_station_status.delta").toPandas()
+station_info = spark.read.format("delta").load("dbfs:/FileStore/tables/bronze_station_info.delta").toPandas()
+weather_data = spark.read.format("delta").load("dbfs:/FileStore/tables/bronze_nyc_weather.delta").toPandas()
 
 # COMMAND ----------
 
-import json
+display(trip_data)
+display(station_info)
+display(weather_data)
 
-# Return Success
-dbutils.notebook.exit(json.dumps({"exit_code": "OK"}))
+# COMMAND ----------
+
+import datetime
+# filtering and cleaning the streaming tables
+filtered_trip_data = trip_data[trip_data['station_id'] == "61c82689-3f4c-495d-8f44-e71de8f04088"]
+filtered_trip_data['last_reported'] = pd.to_datetime(filtered_trip_data['last_reported'], unit='s')
+
+filtered_station_info = station_info[station_info['external_id'] == "61c82689-3f4c-495d-8f44-e71de8f04088"]
+
+# Add weekday_indicator
+weather_data['time'] = pd.to_datetime(weather_data['time'])
+def is_weekday(day):
+    return day.weekday() < 5
+weather_data['weekday_indicator'] = weather_data['time'].apply(is_weekday)
+
+filtered_weather_data = weather_data[['weekday_indicator', 'temp', 'pop', 'time']]
+
+
+display(filtered_trip_data)
+display(filtered_station_info)
+display(weather_data)
+display(filtered_weather_data)
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# Load streaming historical data (net change)
+
+bronze_station_status_oneday = spark.read.format("delta").load("dbfs:/user/hive/warehouse/g09_db.db/bronze_station_status_oneday")
+
+# Convert the Spark DataFrame to a Pandas DataFrame (optional)
+bronze_station_status_oneday_pd = bronze_station_status_oneday.toPandas()
+bronze_station_status_oneday_pd = bronze_station_status_oneday_pd.dropna()
+bronze_station_status_oneday_pd['last_reported'] = pd.to_datetime(bronze_station_status_oneday_pd['last_reported'])
+bronze_station_status_oneday_pd['last_reported'] = bronze_station_status_oneday_pd['last_reported'].dt.round('H')
+
+# Display the DataFrame
+display(bronze_station_status_oneday_pd)
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+import datetime
+import pytz
+import pandas as pd
+import mlflow
+import mlflow.prophet
+from prophet import Prophet
+
+# Load the production model using MLflow
+def load_staging_model():
+    client = mlflow.tracking.MlflowClient()
+    staging_model_version = client.get_latest_versions(GROUP_MODEL_NAME, stages=["Staging"])[0]
+    model_uri = f"models:/{GROUP_MODEL_NAME}/{staging_model_version.version}"
+    return mlflow.prophet.load_model(model_uri)
+
+def prepare_future_df(hours, df_last):
+    future_df = df_last.copy()
+    future_timestamps = [df_last["ds"].max() + datetime.timedelta(hours=i+1) for i in range(hours)]
+    future_df = future_df.append(pd.DataFrame({"ds": future_timestamps}), ignore_index=True)
+    return future_df
+
+# Load the production model
+GROUP_MODEL_NAME = "G09_model"
+model = load_staging_model()
+
+
+filtered_weather_data = filtered_weather_data.rename(columns={"time": "ds"})
+
+# Prepare future dataframe
+hours_to_forecast = 1000
+future_df = prepare_future_df(hours_to_forecast, filtered_weather_data)
+future_df = future_df.dropna()
+# print(future_df)
+# Make predictions
+forecast = model.predict(future_df)
+
+display(forecast)
+# Plot results
+fig = model.plot(forecast)
+plt.show()
+
+# COMMAND ----------
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+#plot residual
+actual_values = bronze_station_status_oneday_pd[['last_reported', 'Net_Change']].set_index('last_reported')
+predicted_values = forecast[['ds', 'yhat']].set_index('ds')
+
+# Filter the actual_values and predicted_values to include only the common dates
+common_dates = actual_values.index.intersection(predicted_values.index)
+actual_values = actual_values.loc[common_dates]
+predicted_values = predicted_values.loc[common_dates]
+
+# Calculate residuals
+residuals = actual_values['Net_Change'] - predicted_values['yhat']
+
+# Create residual plot
+plt.figure(figsize=(10, 6))
+plt.scatter(predicted_values, residuals, alpha=0.5)
+
+# Fit a linear regression model to the residuals
+lr = LinearRegression()
+lr.fit(predicted_values.values.reshape(-1, 1), residuals.values.reshape(-1, 1))
+predicted_residuals = lr.predict(predicted_values.values.reshape(-1, 1))
+
+# Add a line representing the trend of the residuals
+plt.plot(predicted_values, predicted_residuals, color='r', linestyle='--', alpha=0.7, label='Trend Line')
+
+plt.xlabel('Predicted Values')
+plt.ylabel('Residuals')
+plt.title('Residual Plot')
+plt.legend()
+
+plt.show()
+
+
+# COMMAND ----------
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+residuals = actual_values['Net_Change'] - predicted_values['yhat']
+# Create violin plot
+plt.figure(figsize=(10, 6))
+sns.violinplot(x=residuals)
+plt.xlabel('Residuals')
+plt.title('Residuals Distribution')
+
+plt.show()
+
+
+
+
+# COMMAND ----------
+
+import datetime
+import pytz
+
+eastern_tz = pytz.timezone('US/Eastern')
+current_time_eastern = datetime.datetime.now(eastern_tz)
+formatted_datetime = current_time_eastern.strftime('%Y-%m-%d %H:%M:%S')
+
+# Filter the forecast to keep only the data from the current time and future
+forecast = forecast[forecast["ds"] > formatted_datetime]
+
+# print(formatted_datetime)
+# Print the filtered forecast
+# print(forecast)
+
+# COMMAND ----------
+
+# Get the last 5 rows of the forecast DataFrame (including the current hour and the next 4 hours)
+forecast_tail = forecast.head(5)
+
+# COMMAND ----------
+
+import pandas as pd
+
+# Sort filtered_trip_data by 'last_reported' column
+filtered_trip_data_sorted = filtered_trip_data.sort_values(by='last_reported')
+
+# Merge filtered_trip_data_sorted with forecast_tail to find the closest match
+merged_data = pd.merge_asof(
+    forecast_tail, 
+    filtered_trip_data_sorted[['last_reported', 'num_docks_available']], 
+    left_on='ds', 
+    right_on='last_reported', 
+    direction='nearest'
+)
+
+# Subtract the net bike change from the num_docks_available value
+merged_data["adjusted_demand"] = merged_data["num_docks_available"] - merged_data["yhat"]
+
+# Plot the results
+fig, ax = plt.subplots(figsize=(10, 6))
+
+# Plot the line
+ax.plot(merged_data["ds"], merged_data["adjusted_demand"], marker='o', linestyle='-', label='Adjusted Prediction')
+
+# Set the x-axis limits to show only the 4 hours in the future
+ax.set_xlim(merged_data["ds"].min(), merged_data["ds"].max())
+
+# Customize the plot
+ax.set_xlabel("Time")
+ax.set_ylabel("Bikes Available")
+ax.set_title("Station Forecast")
+ax.legend()
+
+# Add a horizontal line at y=83 and label it
+station_capacity = 83
+ax.axhline(y=station_capacity, color='r', linestyle='--', alpha=0.7, label='Station Capacity Line')
+
+# Show the plot
+plt.show()
+
+
+
+# COMMAND ----------
+
+# MAGIC %pip install folium
+
+# COMMAND ----------
+
+import folium
+
+latitude, longitude = 40.7128, -74.0060  # New York City coordinates
+map = folium.Map(location=[latitude, longitude], zoom_start=12)
+marker_latitude, marker_longitude = 40.74322681432173, -73.97449783980846 
+folium.Marker([marker_latitude, marker_longitude], popup='Example Location').add_to(map)
+display(map)
+
+
+# COMMAND ----------
+
+
+
 
 # COMMAND ----------
 
